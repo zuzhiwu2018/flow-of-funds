6fb576c73954f0091d4640c76c42976a43fe54cc
Francesco Ceccon <francesco@ceccon.me>
2023-05-13 17:41:01 +0100

    visualize transfers

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,9 @@
-import { Box, Input } from "@chakra-ui/react";
+import { Box, Circle, Input } from "@chakra-ui/react";
 import { Form, useActionData } from "@remix-run/react";
-import { ActionFunction, LoaderFunction, V2_MetaFunction, json } from "@remix-run/node";
-import { fetchAddressTransfers } from "./transfers.server";
+import { ActionFunction, V2_MetaFunction, json } from "@remix-run/node";
+import { Network, Responsive, Scale } from "@visx/visx";
+import { Suspense, useMemo } from "react";
+import { fetchAddressTransfers, Transfer } from "./transfers.server";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Flow of Funds" }];
@@ -25,6 +27,7 @@ export const action: ActionFunction = async ({ request }) => {
   return json({
     type: 'data',
     transfers,
+    target: address.toString(),
   })
 }
 
@@ -40,6 +43,119 @@ export function SearchBox() {
   )
 }
 
+interface CustomNode {
+  x: number
+  y: number
+  address: string
+  size: number
+  color: string
+}
+
+interface CustomLink {
+  // from: CustomNode
+  // to: CustomNode
+}
+
+function TransferNetwork({ width, height, transfers, target }: { width: number, height: number, transfers: Transfer[], target: string }) {
+  const nodes = useMemo(() => {
+    const amounts = new Map<string, number>()
+    const txCount = new Map<string, number>()
+
+    function addAmount(addr: string, amount: string) {
+      const prev = amounts.get(addr) ?? 0
+      amounts.set(addr, prev + parseInt(amount))
+    }
+
+    function addCount(addr: string) {
+      const prev = txCount.get(addr) ?? 0
+      txCount.set(addr, prev + 1)
+    }
+
+    transfers.forEach((transfer) => {
+      const from = transfer.from.toLowerCase()
+      const to = transfer.to.toLowerCase()
+      addAmount(from, transfer.value)
+      addAmount(to, transfer.value)
+      addCount(from)
+      addCount(to)
+    })
+
+    const angleIncrement = (2.0 * Math.PI) / (amounts.size - 2)
+    const base = Math.min(width, height)
+    const radius = base * 0.3
+
+    const centerX = width / 2.0
+    const centerY = height / 2.0
+
+    const targetAddr = target.toLowerCase()
+    const amountMin = Math.min.apply(null, Array.from(amounts.values()))
+    const amountMax = Math.max.apply(null, Array.from(amounts.values()))
+    const sizeScale = Scale.scaleLinear({
+      domain: [amountMin, amountMax],
+      range: [1, base * 0.15],
+      round: true,
+    })
+
+    const radiusScale = Scale.scaleLinear({
+      domain: [1, 3],
+      range: [radius * 0.5, radius],
+      round: true,
+    })
+
+    return Array.from(amounts.keys()).map((address, idx) => {
+      const value = amounts.get(address)
+      const addr = (address as string).toLowerCase()
+      if (addr === targetAddr) {
+        return {
+          address: addr,
+          x: centerX,
+          y: centerY,
+          size: base * 0.05,
+          color: 'var(--chakra-colors-gray-50)',
+        }
+      }
+
+      console.log(addr, txCount.get(addr))
+      const r = radiusScale(Math.min(txCount.get(addr) ?? 1, 3))
+      return {
+        address: addr,
+        x: centerX + r * Math.cos(angleIncrement * idx),
+        y: centerY + r * Math.sin(angleIncrement * idx),
+        size: sizeScale(value ?? 0),
+        color: 'var(--chakra-colors-blue-500)',
+      }
+    })
+  }, [transfers, target, width, height])
+
+  const links = useMemo(() => {
+    return transfers.map(transfer => ({
+      from: { address: transfer.from },
+      to: { address: transfer.to }
+    }))
+  }, [transfers])
+
+  return (
+    <Network.Graph<CustomLink, CustomNode>
+      graph={{nodes, links}}
+      nodeComponent={({ node: { color, size }}) => <circle fill={color} r={size} />}
+    />
+    )
+}
+
+export function TransferGraph({ target, transfers }: { target: string, transfers: Transfer[] }) {
+  return (
+    <Suspense>
+      <Responsive.ParentSize>
+        {({ width, height }) => (
+          <svg width={width} height={height}>
+            <TransferNetwork width={width} height={height} transfers={transfers} target={target} />
+          </svg>
+        )}
+      </Responsive.ParentSize>
+    </Suspense>
+  )
+}
+
 export default function Index() {
   const data = useActionData<typeof action>()
 
@@ -49,7 +165,7 @@ export default function Index() {
     <Box w="100vw" h="100vh" p="0" m="0" bg="gray.800" display="flex" flexDir="column">
       <SearchBox />
       <Box flex="1" w="full" h="100%">
-        {data?.type === 'data' ? 'ok' : <></>}
+        {data?.type === 'data' ? <TransferGraph target={data.target} transfers={data.transfers} /> : <></>}
       </Box>
     </Box>
   );
--- app/routes/transfers.server.ts
+++ app/routes/transfers.server.ts
@@ -10,6 +10,44 @@ export interface Transfer {
   value: string
 }
 
+const spam = [
+  '$ Visit ensbonus.com to claim',
+  'Invitation token. Please Visit https://atuni.site',
+  '[https://as-uni.org] Visit and claim rewards',
+  'Up1.org',
+]
+
+function toUSD(symbol: string, decimals: string, amount: string): string {
+  if (symbol == 'USDC') {
+    return amount
+  } else if (symbol == 'DAI') {
+    return amount
+  } else if (symbol == 'DYDX') {
+    return (BigInt(amount) * BigInt(2)).toString()
+  } else if (symbol == 'CODE') {
+    return (BigInt(amount) / BigInt(10)).toString()
+  } else if (symbol == 'DPI') {
+    return (BigInt(amount) * BigInt(65)).toString()
+  } else if (symbol == 'FWB') {
+    return (BigInt(amount) * BigInt(4)).toString()
+  } else if (symbol == 'UNI') {
+    return (BigInt(amount) * BigInt(5)).toString()
+  } else if (symbol == 'ENS') {
+    return (BigInt(amount) * BigInt(10)).toString()
+  } else if (symbol == 'MATIC') {
+    return (BigInt(amount) * BigInt(1)).toString()
+  } else if (symbol == 'RAD') {
+    return (BigInt(amount) * BigInt(2)).toString()
+  } else if (symbol == 'BAT') {
+    return (BigInt(amount) / BigInt(5)).toString()
+  } else if (spam.includes(symbol)) {
+    return '0'
+  } else {
+    console.log(symbol, decimals)
+    return '0'
+  }
+}
+
 export async function fetchAddressTransfers(address: string): Promise<Transfer[]> {
   console.log(`fetching transfers for ${address}`)
 
@@ -20,6 +58,7 @@ export async function fetchAddressTransfers(address: string): Promise<Transfer[]
   }
   const transfers = data['result'] ?? []
   return transfers.map((transfer: any) => {
+    // const value = BigInt(transfer.value) / (BigInt(10) ** BigInt(transfer.tokenDecimal))
     return {
       timestamp: transfer.timeStamp,
       contract: transfer.contractAddress,
@@ -27,7 +66,7 @@ export async function fetchAddressTransfers(address: string): Promise<Transfer[]
       hash: transfer.hash,
       from: transfer.from,
       to: transfer.to,
-      value: '0.0'
+      value: toUSD(transfer.tokenSymbol, transfer.tokenDecimal, transfer.value),
     }
   })
 }
